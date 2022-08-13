15995d3aac83dffbdf0682e2057f3e4543437777
Дмитрий Синявин <79263812221@ya.ru>
2022-08-13 10:15:47 +0300

    DZ Base Concepts. Done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,49 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  "use strict"
+
+  let arr = [];
+  let d;
+  
+  d = b**2-(4 * a * c);
+  
+  if (d < 0){
+    arr = [];
+  } else if (d === 0){
+    arr[0] = -b / (2 * a);
+  } else if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d))/(2*a);
+    arr[1] = (-b - Math.sqrt(d))/(2*a);
+  }
+
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  "use strict"
+
   let totalAmount;
+  let error = '';
+  let bodyCredit, months, pMonth;
 
-  // код для задачи №2 писать здесь
+// Проверка на кооректность ввода данных в поля с числами.
+  if (Number.isNaN(Number(percent) + 1) === true){
+    error += "Параметр \"Процентная ставка\" содержит неправильное значение \"" + percent + "\"";
+  }
+  if (Number.isNaN(Number(contribution) + 1) === true){
+    error += "Параметр \"Начальный взнос\" содержит неправильное значение \"" + contribution + "\"";
+  }
+  if (Number.isNaN(Number(amount) + 1) === true){
+    error += "Параметр \"Общая стоимость\" содержит неправильное значение \"" + amount + "\"";
+  }
+  if (error != ''){
+    return error;
+  }
 
-  return totalAmount;
+  bodyCredit = amount - contribution;
+  pMonth = percent / 12 / 100;
+  months = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+  totalAmount = bodyCredit * (pMonth + (pMonth / (Math.pow(1 + pMonth,months) - 1))) * months;
+  
+  return +totalAmount.toFixed(2);
+
 }
