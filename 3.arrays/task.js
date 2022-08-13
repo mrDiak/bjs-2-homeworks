0cb12813fb4a8246b9751c408ed69a272dc68479
Дмитрий Синявин <79263812221@ya.ru>
2022-08-13 17:41:19 +0300

    DZ Arrays. Done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
+  if (arr1.length != arr2.length){
+    return false;
+  } else{
+  result = arr1.every((items, i) => items === arr2[i]);
+  }
 
   return result; // boolean
 }
@@ -9,7 +12,10 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
-
+  resultArr = arr
+    .filter((items) => items > 0)
+    .filter((items) => items % 3 === 0)
+    .map((items) => items * 10);
+    
   return resultArr; // array
 }
