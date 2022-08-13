e268e74625f15df5de422c20e68a4f9a39f4be8c
Дмитрий Синявин <79263812221@ya.ru>
2022-08-13 12:00:44 +0300

    DZ Function. Done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,61 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let 
+    i = 0, 
+    min = arr[0], 
+    max = arr[0], 
+    sum = 0, 
+    avg;
 
-  // Ваш код
+  while (i < arr.length){
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+    sum += arr[i];
+    i++;
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let 
+    sum = 0,
+    i = 0;
+  
+  while (i < arr.length){
+    sum += arr[i]
+    i++;
+  }
 
-  // Ваш код
-
-  return sum;
+  return sum; 
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
-  
+  for (i = 0; i < arrOfArr.length; i++){
+    if (func(arrOfArr[i]) > max) max = func(arrOfArr[i]);
+}
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let 
+    difference = 0,
+    i = 0,
+    min = arr[0],
+    max = arr[0];
+     
+  while (i < arr.length){
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+    difference = Math.abs(max - min);
+    i++;
+  }; 
+  return difference;
 }
