317f67f36bb755b860d3edcc904c21a2bfcdac90
Дмитрий Синявин <79263812221@ya.ru>
2022-08-14 12:15:47 +0300

    DZ Object. Done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
+"Use strict"
+
 function Student(name, gender, age) {
-    // Ваш код
+     this.name = name;
+     this.gender = gender;
+     this.age = age;
+ }
 
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined){ 
+   this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function(...Marks) {
+  if(this.marks === undefined){ 
+    this.marks = [...Marks];
+  } else {
+    this.marks.push(...Marks);
+  }
+}
+
+Student.prototype.getAverage = function() {
+  let avg = 0;
+
+  for (let i = 0; i < this.marks.length; i++) {
+    avg += this.marks[i];
+  }
+
+  return (avg / this.marks.length);
+}
+
+Student.prototype.exclude = function(reason) {
+  this.excluded = reason;
+  
+  if(reason === 'low grades') {
+    delete this.subject;
+    delete this.marks;
+  }
+}
