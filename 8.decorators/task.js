70c2d0bc89849010dadc3988df20b6cf0598365b
Дмитрий Синявин <79263812221@ya.ru>
2022-08-22 22:33:46 +0300

    DZ Decorators. Done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,67 @@
+"use strict"
+
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    const objectInCache = cache.find(item => item.hash === hash);
+
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.result);
+      return "Из кэша: " + objectInCache.result;
+    } else {
+      let result = func.call(this, ...args);
+      if (cache.length > 4) {
+        cache.shift();
+      }
+      cache.push({ hash, result });
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
+
+  return wrapper;
 }
 
+// function debounceDecoratorNew(func) {
+//   let timerId = null;
+//   let firstCall = true;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+//   function wrapper(...args) {
+//     if(!firstCall) {
+//       firstCall = false;
+//       func.apply(this, args);
+//     } else {
+//       clearTimeout(timerId);
+//       timerId = setTimeout(() => {
+//         firstCall = true;
+//         return func.apply(this, args);
+//     })
+//   }
+// }
+//   return wrapper;
+// }
+
+
+function debounceDecoratorNew(func, ms) {
+  let timerID = null;
+  
+  function wrapper (...args) {
+    if (!timerID) {
+      func.apply(this, args);
+      wrapper.count.push(args);
+    }
+
+    clearTimeout(timerID);
+    timerID = setTimeout(() => {
+      func.apply(this, args);
+      wrapper.count.push(args);
+    }, ms)
+    wrapper.allCount++
+  }
+  
+  wrapper.allCount = 0;
+  wrapper.count = [];
+  return wrapper
 }
